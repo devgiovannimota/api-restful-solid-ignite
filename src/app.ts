f30d4671c5f4a646a211330f87e3ea43266a1ba3
Giovanni Mota de Oliveira <devgiovannimota@gmail.com>
2024-03-04 12:40:16 -0300

    last fix

--- src/app.ts
+++ src/app.ts
@@ -9,13 +9,6 @@ import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 
 export const app = fastify();
 
-console.log(
-  "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
-);
-console.log(
-  env.JWT_SECRET +
-    "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-);
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
   cookie: {
